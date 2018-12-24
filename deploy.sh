88ae69dca864f801652ba472fd21d6b183898d63
瞿广 <652658534@qq.com>
2018-12-24 10:15:58 +0800

    add article

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-hugo
-cd ./public
-git init
-git remote add origin  https://github.com/techqu/techqu.github.io.git
+
+git clone  https://github.com/techqu/techqu.github.io.git ./public
 git config --local https.proxy socks5://127.0.0.1:1080
 git config --local http.proxy socks5://127.0.0.1:1080
+hugo
 git add .
 git pull --rebase origin master
 git commit -m "add article"
