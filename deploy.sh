5931cfe4b00f259d4b354f0a1bcd8d6cd785eb9d
瞿广 <652658534@qq.com>
2018-12-24 10:27:40 +0800

    add article

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-git clone  https://github.com/techqu/techqu.github.io.git ./public
+git submodule add   https://github.com/techqu/techqu.github.io.git public
+cd public
 git config --local https.proxy socks5://127.0.0.1:1080
 git config --local http.proxy socks5://127.0.0.1:1080
-hugo
+../hugo
 git add .
-git pull --rebase origin master
+git pull  origin master
 git commit -m "add article"
 git push origin master
 rm -rf ./public/
